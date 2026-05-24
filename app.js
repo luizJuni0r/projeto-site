35302f6c7436377f1c8312608c905e53b84136cf
Luiz Junior <luizdcarlos21@gmail.com>
2026-05-24 14:43:11 -0300

    mais alterações

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ topicos.forEach( topico => {
 	  <div class="col-12 col-md-6 col-lg-4">
 	    <div class="card shadow-sm h-100 d-flex flex-column" style="min-height: 220px;">
 	      
-	      <div class="flex-grow-1 d-flex align-items-center justify-content-center text-center bg-warning p-4">
+	      <div class="flex-grow-1 d-flex align-items-center justify-content-center text-center card-topo rounded-top p-4">
 	        <h4 class="m-0">${topico}</h4>
 	      </div>
 
